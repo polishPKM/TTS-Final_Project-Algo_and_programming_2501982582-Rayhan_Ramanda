703b699181bb3622d5c356db5975b543d2bd1e75
polishPKM <iburehan51@gmail.com>
2022-01-12 12:59:06 +0700

    Revert "Modified and splitted into 2"
    This reverts commit 4edeaaaf107ab7e593954a64c5585f85f16c0b52.

--- TTS.py
+++ TTS.py
@@ -1,40 +1,73 @@
-#importing the module
-from modules import *
+#importing the modules
+from tkinter import *
+from gtts import gTTS
+ 
+import os
+ 
 
-#the graphical user UI , overall
 root = Tk()
-root.geometry("400x400")
-root.title("Text to Speech")
-root.config(bg="white")
+ 
+#yellow upper part of app
+frame1 = Frame(root,
+               bg = "yellow",
+               height = "150")
+ 
+#filling it with colour
+frame1.pack(fill = X)
+ 
+#ditto as 1 but lower part
+frame2 = Frame(root,
+               bg = "red",
+               height = "750")
+frame2.pack(fill=X)
+ 
+ 
+ 
+#app title on upper section
+label = Label(frame1, text = "Text to Speech",
+              font = "bold, 30",
+              bg = "cyan")
+ 
+label.place(x = 180, y = 70)
+ 
+ 
+ 
+entry = Entry(frame2, width = 45,
+              bd = 4, font = 14)
+ 
+entry.place(x = 130, y = 52)
+entry.insert(0, "")
+ 
+#playing the sound
+def play():
+    #which language for TTS used
+    language = "en"
+ 
+ 
+ 
+     #passing the language 
+    obyek = gTTS(text = entry.get(),
+                lang = language,
+                slow = False)
+ 
+ 
+ 
 
-Label(root, text="Text to Speech", bg="white smoke").pack()
-Label(root, text="Enter text:", bg="white smoke").place(x=20, y=60)
+    obyek.save("TTS.wav")
+    os.system("TTS.wav")
+ 
+#submit button to play
+btn = Button(frame2, text = "SUBMIT",
+             width = "15", pady = 10,
+             font = "bold, 15",
+             command = play, bg='yellow')
+ 
+btn.place(x = 250,
+          y = 130)
+ 
+root.title("TTS") #App name on window bar
 
-
-#to read the text on the typing box
-Msg = StringVar()
-
-#the typing box itself
-input_field = Entry(root, textvariable=Msg, width="40")
-input_field.place(x=20, y=100)
-
-#TTS functions
-def tts():
-    message = input_field.get()
-    speech = gTTS(text=message)
-    speech.save("speech.mp3")
-    playsound("speech.mp3")
-
-def Exit():
-    root.destroy()
-
-def Reset():
-    Msg.set("")
-
-
-Button(root, text="Play", command=tts, width=3).place(x=20, y=140)
-Button(root, text="Exit", command=Exit, width=3).place(x=80, y=140)
-Button(root, text="Reset", command=Reset, width=3).place(x=140, y=140)
-
-
+ 
+root.geometry("650x550+350+200")
+ 
 root.mainloop()
--- modules.py
+++
@@ -1,5 +0,0 @@
-#modules we need
-from typing import Text
-from gtts import gTTS
-from playsound import playsound
-from tkinter import Tk, Label, StringVar, Entry, Button
